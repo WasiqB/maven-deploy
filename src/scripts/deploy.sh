44d07b506dd88ec17d3a40acdfdbae26e58763d3
WasiqB <wasbhamla2005@gmail.com>
2020-08-29 18:10:56 +0530

    Updated Orb to enable Auto deployment to Maven central.

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,3 +1,18 @@
+case "$SEMVER_INCREMENT" in
+"major")
+  VERSION_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.0.0 versions:commit"
+  VERSION_SNAPSHOT_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT versions:commit"
+  ;;
+"minor")
+  VERSION_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.0 versions:commit"
+  VERSION_SNAPSHOT_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0-SNAPSHOT versions:commit"
+  ;;
+"patch")
+  VERSION_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit"
+  VERSION_SNAPSHOT_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit"
+  ;;
+esac
+
 echo "Starting new Release snapshot..."
 eval "$VERSION_SNAPSHOT_COMMAND"
 MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
@@ -9,12 +24,3 @@ eval "$VERSION_COMMAND"
 MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
 echo "Releasing version ${MVN_VERSION}..."
 eval "$DEPLOY_COMMAND"
-
-echo "Pushing new version and tag..."
-rm .circleci/gpg-ci
-git add pom.xml
-git commit -m "Released v${MVN_VERSION} [skip ci]"
-git tag -s v${MVN_VERSION} -m "Released v${MVN_VERSION}"
-ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa; git push $CIRCLE_REPOSITORY_URL'
-ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa; git push origin --tags'
-echo "Successfully released ${MVN_VERSION}"
---
+++ src/scripts/push-version.sh
@@ -0,0 +1,8 @@
+echo "Pushing new version and tag..."
+rm .circleci/gpg-ci
+git add pom.xml
+git commit -m "Released v${MVN_VERSION} [skip ci]"
+git tag -s v${MVN_VERSION} -m "Released v${MVN_VERSION}"
+ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa; git push $CIRCLE_REPOSITORY_URL'
+ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa; git push origin --tags'
+echo "Successfully released ${MVN_VERSION}"
