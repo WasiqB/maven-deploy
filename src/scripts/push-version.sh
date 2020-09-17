0990ad3c3ea592c3df88b1f830290f5dbe407cc4
WasiqB <wasbhamla2005@gmail.com>
2020-09-17 17:13:42 +0530

    fix (deploy): Issue while pushing version tag to git.

--- src/scripts/push-version.sh
+++ src/scripts/push-version.sh
@@ -2,10 +2,10 @@ if [ -z "${VERSION_INCREMENT}" ] || [ "$VERSION_INCREMENT" == "skip" ]; then
   exit 0
 fi
 echo "Pushing new version and tag..."
-rm .circleci/gpg-ci
 git add pom.xml
 git commit -m "Released v${MVN_VERSION} [skip ci]"
 git tag -s v${MVN_VERSION} -m "Released v${MVN_VERSION}"
 ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa; git push $CIRCLE_REPOSITORY_URL'
 ssh-agent sh -c 'ssh-add ~/.ssh/id_rsa; git push origin --tags'
+rm .circleci/gpg-ci
 echo "Successfully released ${MVN_VERSION}"
