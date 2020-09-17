797aa4c0dff02dd81542f0e896e977076b1868df
Wasiq Bhamla <wasbhamla2005@gmail.com>
2020-09-17 11:41:48 +0530

    [semver:patch] Fixed deploy issue.

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,29 +1,55 @@
-case "$VERSION_INCREMENT" in
-"major")
-  VERSION_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.0.0 versions:commit"
-  VERSION_SNAPSHOT_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT versions:commit"
-  ;;
-"minor")
-  VERSION_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.0 versions:commit"
-  VERSION_SNAPSHOT_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0-SNAPSHOT versions:commit"
-  ;;
-"patch")
-  VERSION_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit"
-  VERSION_SNAPSHOT_COMMAND="mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit"
-  ;;
-*)
-  exit 0
-  ;;
-esac
-
-echo "Starting new Release snapshot..."
-eval "$VERSION_SNAPSHOT_COMMAND"
-MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
-echo "Releasing Snapshot version ${MVN_VERSION}..."
-eval "$DEPLOY_COMMAND"
-
-echo "Starting new Release..."
-eval "$VERSION_COMMAND"
-MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
-echo "Releasing version ${MVN_VERSION}..."
-eval "$DEPLOY_COMMAND"
+setMavenVersion() {
+  MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
+  echo "export MVN_VERSION=\"$MVN_VERSION\"" >>"$BASH_ENV"
+}
+setVersionCommand() {
+  case "$VERSION_INCREMENT" in
+  "major")
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.0.0 versions:commit
+    ;;
+  "minor")
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.0 versions:commit
+    ;;
+  "patch")
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit
+    ;;
+  *)
+    exit 0
+    ;;
+  esac
+  setMavenVersion
+}
+setVersionCommandSnapshot() {
+  case "$VERSION_INCREMENT" in
+  "major")
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT versions:commit
+    ;;
+  "minor")
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0-SNAPSHOT versions:commit
+    ;;
+  "patch")
+    mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
+    ;;
+  *)
+    exit 0
+    ;;
+  esac
+  setMavenVersion
+}
+deploySnapshot() {
+  echo "Starting new Release snapshot..."
+  setVersionCommandSnapshot
+  echo "Releasing Snapshot version ${MVN_VERSION}..."
+  eval "${DEPLOY_COMMAND}"
+}
+deploy() {
+  echo "Starting new Release..."
+  setVersionCommand
+  echo "Releasing version ${MVN_VERSION}..."
+  eval "${DEPLOY_COMMAND}"
+}
+main() {
+  deploySnapshot
+  deploy
+}
+main
